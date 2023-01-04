185f8e435d5540ee85120d37004bc35c9de92524
NMoz88 <nesmartinez2@gmail.com>
2023-01-04 02:36:42 +0000

    swapi api flask

--- src/models.py
+++ src/models.py
@@ -17,3 +17,45 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+class Fav_People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    people_name = db.Column(db.String(120), db.ForeignKey("people.name"))
+    user_fav = db.Column(db.String(120), db.ForeignKey("user.email"))
+    rel_people = db.relationship("People")
+    rel_user = db.relationship("User")
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    Diameter = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.Diameter,
+        }
+
+class Fav_Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planets_name = db.Column(db.String(120), db.ForeignKey("planets.name"))
+    user_fav = db.Column(db.String(120), db.ForeignKey("user.email"))
+    rel_planets = db.relationship("Planets")
+    rel_user = db.relationship("User")
